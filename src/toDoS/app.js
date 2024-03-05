bfb9153d33abcea7f5786c98ad8d46b6cf2c1009
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-05 00:32:02 -0300

    se implementa inserción de nuevos toDos

--- src/toDoS/app.js
+++ src/toDoS/app.js
@@ -3,8 +3,9 @@ import html from "./app.html?raw";
 import { renderToDos } from "./use-cases";
 
 const ElementHTMLIDs = {
-    TodoList : '.todo-list',
-}
+    TodoList: ".todo-list",
+    NewToDoInput: "#new-todo-input",
+};
 
 /**
  *
@@ -25,6 +26,28 @@ export const App = (elementIdRenderizaFuncionApp) => {
         document.querySelector(elementIdRenderizaFuncionApp).append(divApp);
         displayTodo();
     })();
+
+
+    // Referencias HTML
+    const newDescriptionInput = document.querySelector( ElementHTMLIDs.NewToDoInput )
+
+    // Listeners
+    newDescriptionInput.addEventListener('keyup', ( event ) => {
+        console.log(event)
+        console.log(event.target.value)
+
+        if ( event.keyCode !== 13 ) return;
+        if ( event.target.value.trim().length === 0 ) return; 
+
+        todoStore.addTodo( event.target.value ); 
+        displayTodo();
+        event.target.value = ' '
+    })
+
+
+
+
+    
 };
 
 //TODO revisar que pasa si coloco ese html directamente a #app
