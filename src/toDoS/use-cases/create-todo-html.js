312e6f6dc29a1aa9ee86b6b9ccb655e956a98a61
CarlosMoralesDev23 <carlosmoralesdev23@gmail.com>
2024-03-04 23:02:28 -0300

    se refactorizacodigo y se corrige error de description en renderizado de toDos

--- src/toDoS/use-cases/create-todo-html.js
+++ src/toDoS/use-cases/create-todo-html.js
@@ -1,16 +1,30 @@
 import { Todo } from "../models/todo.model";
 
 
-
+/**
+ * 
+ * @param {Todo} toDo 
+ * @returns 
+ */
 export const createToDoHTML = ( toDo )=> {
 
-    if ( !Todo ) throw new Error(' A TODO object is required')
+    if ( !toDo ) throw new Error(' A TODO object is required')
 
-    const html = `<h3> ${toDo.description} </h3> `
+    const html = `
+        <li class="completed" data-id="abc">
+            <div class="view">
+                <input class="toggle" type="checkbox" ${ toDo.done ? 'checked': ''} >
+                <label> ${ toDo.description } </label>
+                <button class="destroy"></button>
+            </div>
+            <input class="edit" value="Create a TodoMVC template">
+        </li>`;
 
     const  liElement = document.createElement('li');
-
     liElement.innerHTML = html
+    liElement.setAttribute('data-id', toDo.id)
+    if( Todo.done )
+    liElement.classList( 'completed ')
 
     return liElement
 
--- src/toDoS/use-cases/render-todos.js
+++ src/toDoS/use-cases/render-todos.js
@@ -7,11 +7,11 @@ import { Todo } from "../models/todo.model"
  * @param {String} elementId etiqueta donde renderizar los todos 
  * @param {Todo} toDos  
  */
-export const renderToDos = (ElementHTMLIDs, toDos = []) => {
+export const renderToDos = ( ElementHTMLIDs, toDos = [] ) => {
     //TODO referencia
     const element = document.querySelector(ElementHTMLIDs);
 
-    toDos.forEach((toDo) => {
+    toDos.forEach( toDo => {
         element.append(createToDoHTML(toDo));
     });
 };
